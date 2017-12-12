05ea3f75e9b8e3f558e9b6814658949adbbebc8f
Admin <admin@Admins-MacBook-Air.local>
2017-12-12 17:52:31 -0500

    attempt at fixing app

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
   JS: path.resolve(__dirname, 'src/js'),
