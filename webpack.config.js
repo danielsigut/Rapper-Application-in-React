c92eb95612bfaf31eefe67ab39f20c53c54d0586
Admin <admin@Admins-MacBook-Air.local>
2017-12-12 19:34:14 -0500

    maybenot

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,7 @@ module.exports = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
-      }
+       'process.env.NODE_ENV': JSON.stringify('production')
     }),
       new HtmlWebpackPlugin({
         template: path.join(paths.SRC, 'index.html'),
