769e9edf589a77c5e5a2e09629a7bdba8c16ef53
Admin <admin@Admins-MacBook-Air.local>
2017-12-12 19:18:29 -0500

    another attempt to fix this app

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
-
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
@@ -19,17 +18,39 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      }
+    }),
       new HtmlWebpackPlugin({
         template: path.join(paths.SRC, 'index.html'),
       }),
       new ExtractTextPlugin('style.bundle.css'),
+      new webpack.optimize.AggressiveMergingPlugin(),
+     new webpack.optimize.OccurrenceOrderPlugin(),
+     new webpack.optimize.UglifyJsPlugin({
+           mangle: true,
+         compress: {
+           warnings: false, // Suppress uglification warnings
+           pure_getters: true,
+           unsafe: true,
+           unsafe_comps: true,
+           screw_ie8: true
+         },
+         output: {
+           comments: false,
+         },
+         exclude: [/\.min\.js$/gi] // skip pre-minified libs
+       }),
+       new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]), //https://stackoverflow.com/questions/25384360/how-to-prevent-moment-js-from-loading-locales-with-webpack
   ],
 
   module: {
   rules: [
     {
       test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
+      exclude: /node_modules(?!\/webpack-dev-server)/,
       use: [
         'babel-loader',
       ],
